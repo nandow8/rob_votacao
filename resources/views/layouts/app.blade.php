a774271973a75ad6ecee9ee9a6b177ff98967f84
Fernando Alves <alvesfernandosantos1@gmail.com>
2020-02-26 10:05:20 -0300

    index strub

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
 
         <!-- Argon CSS -->
         <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('argon') }}/vendor/animate.css/animate.min.css">
+        <link rel="stylesheet" href="{{ asset('argon') }}/vendor/sweetalert2/dist/sweetalert2.min.css">
     </head>
     <body class="{{ $class ?? '' }}">
         @auth()
@@ -46,6 +48,63 @@
         <script src="{{ asset('argon') }}/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
         <script src="{{ asset('argon') }}/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
         <script src="{{ asset('argon') }}/vendor/lavalamp/js/jquery.lavalamp.min.js"></script>
+        <script src="{{ asset('argon') }}/vendor/sweetalert2/dist/sweetalert2.min.js"></script>
+        <script src="{{ asset('argon') }}/vendor/bootstrap-notify/bootstrap-notify.min.js"></script>
+        <script type="text/javascript">
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+        
+            var BASE_URL = '{{url('')}}/';
+        </script>
+
+        <script>
+            $(".btnDeletar").click(function(){
+                _this = this;
+                swal.fire({
+                    title: 'Atenção?',
+                    text: "Deseja remover este item da lista?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Sim!'
+                }).then(function(result){
+                    if (result.value) {
+                        $.ajax({
+                            url: $(_this).attr('data-route') + '/' + $(_this).attr('data-id'),
+                            type: 'POST',
+                            data : {'_method' : 'DELETE'},
+                            dataType: 'JSON',
+                            success: function(result){
+                                if(result == 1) {
+                                    swal({
+                                        title: "Removido(a) com sucesso",
+                                        icon: "success"
+                                    });
+                                    $(_this).closest('tr').remove();
+                                }
+                                else{
+                                    swal({
+                                        title: "Erro ao remover",
+                                        icon: "error"
+                                    });
+                                }
+                            },
+                            error: function(result){
+                                swal({
+                                    title: "Erro remover",
+                                    text: result.responseJSON.message,
+                                    icon: "error"
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        </script>
         
         @stack('js')
 
