f8b94149ea8ddc179c8ebdac0302d1a91968678a
Fernando Alves <fernando.alves@superteia.com.br>
2020-08-01 10:32:29 -0300

    imagem

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -195,7 +195,7 @@
                     <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <div class="media align-items-center">
                             <span class="avatar avatar-sm rounded-circle">
-                                <img alt="Image placeholder" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png">
+                                <img alt="Image placeholder" src="{{ asset('images/user/logoIco.fw.png') }}">
                             </span>
                             <div class="media-body ml-2 d-none d-lg-block">
                                 <span class="mb-0 text-sm  font-weight-bold">{{ auth()->user()->name }}</span>
